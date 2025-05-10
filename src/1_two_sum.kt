fe8b7bddf808a1dbb3050b09dbbaa6c7712d2112
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-05-10 20:13:52 +0600

    Add solution for 1. Two Sum

---
+++ src/1_two_sum.kt
@@ -0,0 +1,23 @@
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val map = mutableMapOf<Int, Int>()
+
+        for ((index, value) in nums.withIndex()) {
+            val complement = target - value
+            if (map.containsKey(complement)) {
+                return intArrayOf(map[complement]!!, index)
+            }
+            map[value] = index
+        }
+
+        throw IllegalArgumentException("No two sum solution found")
+    }
+}
+
+fun main() {
+    val solution = Solution()
+    val nums = intArrayOf(2, 7, 11, 15)
+    val target = 9
+    val result = solution.twoSum(nums, target)
+    println("Indices: ${result[0]}, ${result[1]}")
+}
