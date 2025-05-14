88dedaad7ed6531a8a28f761f915eecfc88c17ba
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-05-14 12:45:02 +0600

    Refactor project structure: organize problems by difficulty and clean up IDE files

---
+++ easy/374. Guess Number Higher or Lower.kt
@@ -0,0 +1,19 @@
+/**
+ * The API guess is defined in the parent class.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * fun guess(num:Int):Int {}
+ */
+
+class Solution/*:GuessGame()*/ {
+    /*override */fun guessNumber(n:Int):Int {
+        return 6
+    }
+}
+
+fun main() {
+    val solution = Solution()
+    println(solution.guessNumber(10))
+}
