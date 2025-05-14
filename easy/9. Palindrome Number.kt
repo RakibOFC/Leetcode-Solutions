71f3aefda071a97d242246024c9021af4361842b
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-05-15 01:11:36 +0600

    Add a duplicate solution for 9. Palindrome Number.kt

--- easy/9. Palindrome Number.kt
+++ easy/9. Palindrome Number.kt
@@ -1,3 +1,4 @@
+// Solution 1.
 class Solution {
     fun isPalindrome(x: Int): Boolean {
         if (x < 0 || (x % 10 == 0 && x != 0)) return false
@@ -11,5 +12,22 @@ class Solution {
         }
 
         return original == reversed || original == reversed / 10
+    }
+}
+
+// Solution 2.
+class Solution {
+    fun isPalindrome(x: Int): Boolean {
+        if (x < 0) return false
+
+        var original = x
+        var reverse = 0
+
+        while (original > 0) {
+            val reminder = original % 10
+            original /= 10
+            reverse = reverse * 10 + reminder
+        }
+        return reverse == x
     }
 }
