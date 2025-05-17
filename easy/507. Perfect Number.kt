f6b8d5d4565b85e10e8713a9834acea5a3f4a7be
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-05-17 16:13:12 +0600

    Add solution 2 for 507. Perfect Number

--- easy/507. Perfect Number.kt
+++ easy/507. Perfect Number.kt
@@ -1,3 +1,4 @@
+// Solution 1.
 class Solution {
     fun checkPerfectNumber(num: Int): Boolean {
         var sum = 0
@@ -9,5 +10,25 @@ class Solution {
             }
         }
         return num == sum
+    }
+}
+
+
+// Solution 2.
+class Solution {
+    fun checkPerfectNumber(num: Int): Boolean {
+        if (num <= 1) return false
+        var sum = 1
+
+        for (i in 2..Math.sqrt(num.toDouble()).toInt()) {
+            if (num % i == 0) {
+                sum += i
+                val other = num / i
+                if (other != i) {
+                    sum += other
+                }
+            }
+        }
+        return sum == num
     }
 }
