c74f34b7e30c74927fc24464c741c63621099c35
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-05-21 10:50:14 +0600

    Add solution for 412. Fizz Buzz

---
+++ easy/412. Fizz Buzz.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun fizzBuzz(n: Int): List<String> {
+        val result = mutableListOf<String>()
+        
+        for (i in 1..n) {
+            val value = when {
+                i % 3 == 0 && i % 5 == 0 -> "FizzBuzz"
+                i % 3 == 0 -> "Fizz"
+                i % 5 == 0 -> "Buzz"
+                else -> i.toString()
+            }
+            result.add(value)
+        }
+        return result
+    }
+}
