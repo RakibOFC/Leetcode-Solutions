9f7815fedc5c9777d315c7113ef4ffeab83e67b3
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-05-24 11:13:07 +0600

    Add solution 2, 3 for 1859. Sorting the Sentence

--- easy/1859. Sorting the Sentence.kt
+++ easy/1859. Sorting the Sentence.kt
@@ -1,3 +1,45 @@
+// Solution 1. (92 ms)
+class Solution {
+    fun sortSentence(s: String): String {
+        val words = s.split(' ')
+        val sorted = Array(words.size) { "" }
+
+        for (i in words.indices) {
+            val word = words[i]
+            val len = word.length
+            val idx = word[len - 1] - '1'
+            sorted[idx] = word.substring(0, len - 1)
+        }
+
+        val lastIndex = sorted.size - 1
+        val sb = StringBuilder(s.length)
+        for (i in 0..<lastIndex) {
+            sb.append(sorted[i]).append(' ')
+        }
+        sb.append(sorted[lastIndex])
+
+        return sb.toString()
+    }
+}
+
+// Solution 2. (103 ms)
+class Solution {
+    fun sortSentence(s: String): String {
+        val map = mutableMapOf<Int, String>()
+        var maxIndex = 0
+
+        for (word in s.split(" ")) {
+            val len = word.length
+            val index = word[len - 1] - '0'
+            map[index] = word.substring(0, len - 1)
+            if (index > maxIndex) maxIndex = index
+        }
+
+        return (1..maxIndex).joinToString(" ") { map[it] ?: "" }
+    }
+}
+
+// Solution 3. (124 ms)
 class Solution {
     fun sortSentence(s: String): String {
         val newSentence: StringBuilder = StringBuilder()
