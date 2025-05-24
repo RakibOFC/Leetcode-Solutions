656a8bb8cee27ad233422a60484bfc0ef09e1b85
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-05-24 12:04:01 +0600

    Add solution 2 for 26. Remove Duplicates from Sorted Array

--- easy/26. Remove Duplicates from Sorted Array.kt
+++ easy/26. Remove Duplicates from Sorted Array.kt
@@ -1,3 +1,22 @@
+// Solution 1.
+class Solution {
+    fun removeDuplicates(nums: IntArray): Int {
+        if (nums.isEmpty()) return 0
+
+        var k = 1
+
+        for (i in 1 until nums.size) {
+            if (nums[i] != nums[k - 1]) {
+                nums[k] = nums[i]
+                k++
+            }
+        }
+
+        return k
+    }
+}
+
+// Solution 2.
 class Solution {
     fun removeDuplicates(nums: IntArray): Int {
         var prevNum = -101
