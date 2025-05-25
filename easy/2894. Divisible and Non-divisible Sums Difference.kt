f302bfae2313e687e2c1ced4ac231af44d08022a
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-05-25 16:51:09 +0600

    Add solution 2 for 2894. Divisible and Non-divisible Sums Difference

--- easy/2894. Divisible and Non-divisible Sums Difference.kt
+++ easy/2894. Divisible and Non-divisible Sums Difference.kt
@@ -1,3 +1,15 @@
+// Solution 2.
+class Solution {
+    fun differenceOfSums(n: Int, m: Int): Int {
+        val totalSum = n * (n + 1) / 2
+        val count = n / m
+        val num2 = m * count * (count + 1) / 2
+        val num1 = totalSum - num2
+        return num1 - num2
+    }
+}
+
+// Solution 1.
 class Solution {
     fun differenceOfSums(n: Int, m: Int): Int {
         var num1 = 0
