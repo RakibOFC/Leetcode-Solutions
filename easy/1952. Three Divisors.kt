ed4416d825054b80c3c72fc198a4091d0fc18400
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-05-25 13:58:30 +0600

    Add solution 2 for 1952. Three Divisors

--- easy/1952. Three Divisors.kt
+++ easy/1952. Three Divisors.kt
@@ -1,6 +1,25 @@
 // Solution 1.
 class Solution {
     fun isThree(n: Int): Boolean {
+        val root = sqrt(n.toDouble()).toInt()
+
+        if (root * root != n) return false
+
+        return isPrime(root)
+    }
+
+    private fun isPrime(num: Int): Boolean {
+        if (num < 2) return false
+        for (i in 2..sqrt(num.toDouble()).toInt()) {
+            if (num % i == 0) return false
+        }
+        return true
+    }
+}
+
+// Solution 2.
+class Solution {
+    fun isThree(n: Int): Boolean {
         var count = 0
         for (i in 1..n) {
             if (n % i == 0) count++
@@ -15,7 +34,7 @@ class Solution {
     }
 }
 
-// Solution 2.
+// Solution 3.
 class Solution {
     fun isThree(n: Int): Boolean {
         var count = 0
