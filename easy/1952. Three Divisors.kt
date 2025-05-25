bff578a7020cb6b39c8efe9352c7560783c51740
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-05-25 11:20:18 +0600

    Add solution 2 for 1952. Three Divisors

--- easy/1952. Three Divisors.kt
+++ easy/1952. Three Divisors.kt
@@ -1,3 +1,21 @@
+// Solution 1.
+class Solution {
+    fun isThree(n: Int): Boolean {
+        var count = 0
+        for (i in 1..n) {
+            if (n % i == 0) count++
+
+            if (count > 3) {
+                break
+            } else {
+                continue
+            }
+        }
+        return count == 3
+    }
+}
+
+// Solution 2.
 class Solution {
     fun isThree(n: Int): Boolean {
         var count = 0
