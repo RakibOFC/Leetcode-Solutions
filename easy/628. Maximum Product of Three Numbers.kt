6ae81a5022a7b93dbf4e1d583e8e3d24c65b9c75
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-05-26 12:28:19 +0600

    Add solution 2 for 628. Maximum Product of Three Numbers

--- easy/628. Maximum Product of Three Numbers.kt
+++ easy/628. Maximum Product of Three Numbers.kt
@@ -1,3 +1,43 @@
+// Solution 2.
+class Solution {
+    fun maximumProduct(nums: IntArray): Int {
+        var max1 = Int.MIN_VALUE
+        var max2 = Int.MIN_VALUE
+        var max3 = Int.MIN_VALUE
+        var min1 = Int.MAX_VALUE
+        var min2 = Int.MAX_VALUE
+
+        for (num in nums) {
+
+            if (num > max1) {
+                max3 = max2
+                max2 = max1
+                max1 = num
+
+            } else if (num > max2) {
+                max3 = max2
+                max2 = num
+
+            } else if (num > max3) {
+                max3 = num
+            }
+
+            if (num < min1) {
+                min2 = min1
+                min1 = num
+            } else if (num < min2) {
+                min2 = num
+            }
+        }
+        
+        val product1 = max1 * max2 * max3
+        val product2 = min1 * min2 * max1
+
+        return if (product1 > product2) product1 else product2
+    }
+}
+
+// Solution 1.
 class Solution {
     fun maximumProduct(nums: IntArray): Int {
         nums.sort()
