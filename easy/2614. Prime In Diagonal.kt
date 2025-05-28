d883b8bc7972ac6ccad3100c627e604faa7afb10
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-05-28 16:10:31 +0600

    Add solution for 2614. Prime In Diagonal

---
+++ easy/2614. Prime In Diagonal.kt
@@ -0,0 +1,29 @@
+class Solution {
+    fun diagonalPrime(nums: Array<IntArray>): Int {
+        var maxPrime = 0
+
+        for (i in nums.indices) {
+            val num1 = nums[i][i]
+            val num2 = nums[i][nums.size - i - 1]
+
+            if (isPrime(num1)) maxPrime = maxNum(num1, maxPrime)
+            if (num1 != num2 && isPrime(num2)) maxPrime = maxNum(num2, maxPrime)
+
+        }
+        return maxPrime
+    }
+
+    private fun maxNum(n1: Int, n2: Int): Int {
+        return if (n1 > n2) n1 else n2
+    }
+
+    private fun isPrime(num: Int): Boolean {
+        if (num < 2) return false
+
+        for (i in 2..sqrt(num.toDouble()).toInt()) {
+            if (num % i == 0) return false
+        }
+
+        return true
+    }
+}
