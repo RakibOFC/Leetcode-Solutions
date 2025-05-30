796aac208587fd2be10467ba754c52dc0a154581
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-05-30 23:12:29 +0600

    Add solution 2 for 1051. Height Checker

--- easy/1051. Height Checker.kt
+++ easy/1051. Height Checker.kt
@@ -1,3 +1,21 @@
+// Solution 2. (4ms)
+class Solution {
+    fun heightChecker(heights: IntArray): Int {
+        val expected = heights.copyOf()
+        expected.sort()
+
+        var count = 0
+        for (i in heights.indices) {
+            if (heights[i] != expected[i]) {
+                count++
+            }
+        }
+
+        return count
+    }
+}
+
+// Solution 1. (14ms)
 class Solution {
     fun heightChecker(heights: IntArray): Int {
         val expected = heights.sorted()
