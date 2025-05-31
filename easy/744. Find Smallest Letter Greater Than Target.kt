58ceb0cac326498a8ca5be7fcb3afac07a1a22ee
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-05-31 12:04:16 +0600

    Add solution 2 for 744. Find Smallest Letter Greater Than Target

--- easy/744. Find Smallest Letter Greater Than Target.kt
+++ easy/744. Find Smallest Letter Greater Than Target.kt
@@ -1,3 +1,23 @@
+// Solution 2. (Binary Search)
+class Solution {
+    fun nextGreatestLetter(letters: CharArray, target: Char): Char {
+        var low = 0
+        var high = letters.size - 1
+
+        while (low <= high) {
+            val mid = low + (high - low) / 2
+            if (letters[mid] <= target) {
+                low = mid + 1
+            } else {
+                high = mid - 1
+            }
+        }
+
+        return letters[low % letters.size]
+    }
+}
+
+// Solution 1. 
 class Solution {
     fun nextGreatestLetter(letters: CharArray, target: Char): Char {
         val smallestChar = letters[0]
