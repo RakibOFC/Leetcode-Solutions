876ecde395df7bbb9ad51ccbd8d18c6f73b4db8c
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-06-01 16:13:23 +0600

    Add solution for 2000. Reverse Prefix of Word.kt

--- easy/2000. Reverse Prefix of Word.kt
+++ easy/2000. Reverse Prefix of Word.kt
@@ -1,3 +1,24 @@
+// Solution 2. (6ms)
+class Solution {
+    fun reversePrefix(word: String, ch: Char): String {
+        val idx = word.indexOf(ch)
+        if (idx == -1) return word
+
+        val arr = word.toCharArray()
+        var i = 0
+        var j = idx
+        while (i < j) {
+            val tmp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = tmp
+            i++
+            j--
+        }
+        return String(arr)
+    }
+}
+
+// Solution 1. (7ms)
 class Solution {
         fun reversePrefix(word: String, ch: Char): String {
         var reversePrefix = word
