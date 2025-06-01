943dad40ffc6c2c764fb18719e459c255bee1278
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-06-01 14:39:47 +0600

    Add solution 2 for 1920. Build Array from Permutation

--- easy/1920. Build Array from Permutation.kt
+++ easy/1920. Build Array from Permutation.kt
@@ -1,3 +1,21 @@
+// Solution 2. (2ms) This is follow-up solution
+class Solution {
+    fun buildArray(nums: IntArray): IntArray {
+        val n = nums.size
+
+        for (i in nums.indices) {
+            nums[i] = nums[i] + (nums[nums[i]] % n) * n
+        }
+
+        for (i in nums.indices) {
+            nums[i] = nums[i] / n
+        }
+
+        return nums
+    }
+}
+
+// Solution 1. (1ms) This is not follow-up solution
 class Solution {
     fun buildArray(nums: IntArray): IntArray {
         val ans = IntArray(nums.size)
