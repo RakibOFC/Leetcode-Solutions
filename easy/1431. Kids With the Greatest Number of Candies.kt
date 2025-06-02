555a3061e891736c8d73e9be6a65dc558a2bfdb0
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-06-02 11:42:01 +0600

    Add solution 2 for 1431. Kids With the Greatest Number of Candies.kt

--- easy/1431. Kids With the Greatest Number of Candies.kt
+++ easy/1431. Kids With the Greatest Number of Candies.kt
@@ -1,3 +1,21 @@
+// Solution 2. (1ms)
+class Solution {
+    fun kidsWithCandies(candies: IntArray, extraCandies: Int): List<Boolean> {
+        var max = 0
+        for (c in candies) {
+            if (c > max) max = c
+        }
+
+        val result = ArrayList<Boolean>(candies.size)
+        for (c in candies) {
+            result.add(c + extraCandies >= max)
+        }
+
+        return result
+    }
+}
+
+// Solution 1. (10ms)
 fun kidsWithCandies(candies: IntArray, extraCandies: Int): List<Boolean> {
         val max = candies.max()
         val result = mutableListOf<Boolean>()
