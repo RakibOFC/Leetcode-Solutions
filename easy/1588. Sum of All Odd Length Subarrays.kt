5f966d8f7363c2aeed6536d192e1aee9202d1067
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-06-02 14:00:55 +0600

    Add solution 2 for 1588. Sum of All Odd Length Subarrays.kt

--- easy/1588. Sum of All Odd Length Subarrays.kt
+++ easy/1588. Sum of All Odd Length Subarrays.kt
@@ -1,3 +1,32 @@
+// Solution 2. (4ms)
+class Solution {
+    fun sumOddLengthSubarrays(arr: IntArray): Int {
+        var sum = 0
+        val n = arr.size
+
+        for (size in 1..n) {
+            if (size % 2 != 0) {
+
+                for (i in 0..<n) {
+                    val limit = size + i - 1
+
+                    if (limit < n) {
+
+                        for (j in i..limit) {
+                            sum += arr[j]
+                        }
+                    } else {
+                        break
+                    }
+                }
+            }
+        }
+
+        return sum
+    }
+}
+
+// Solution 1. (13ms)
 class Solution {
     fun sumOddLengthSubarrays(arr: IntArray): Int {
         var sum = 0
