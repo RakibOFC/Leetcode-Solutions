c3529b6ba87ba22078b7b711ad58f6c6e6b24dd6
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-06-02 15:58:31 +0600

    Add solution 2 for 1470. Shuffle the Array.kt

--- easy/1470. Shuffle the Array.kt
+++ easy/1470. Shuffle the Array.kt
@@ -1,3 +1,19 @@
+// Solution 2. (145ms)
+fun shuffle(nums: IntArray, n: Int): IntArray {
+        val newArr = IntArray(n * 2)
+        var i = 0
+        var j = n
+        var k = 0
+
+        for (l in 1..n) {
+            newArr[k++] = nums[i++]
+            newArr[k++] = nums[j++]
+        }
+
+        return newArr
+    }
+
+// Solution 1. (151ms)
 class Solution {
     fun shuffle(nums: IntArray, n: Int): IntArray {
         val newArr = IntArray(n * 2)
