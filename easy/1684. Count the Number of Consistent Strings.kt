7d0818702d60da08e77da61911b5d3ee71446365
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-06-04 00:32:00 +0600

    Add solution for 1684. Count the Number of Consistent Strings.kt

--- easy/1684. Count the Number of Consistent Strings.kt
+++ easy/1684. Count the Number of Consistent Strings.kt
@@ -1,3 +1,20 @@
+// Solution 2. (213ms)
+class Solution {
+    fun countConsistentStrings(allowed: String, words: Array<String>): Int {
+        val allowedSet = allowed.toSet()
+        var counter = 0
+
+        for (word in words) {
+            if (word.all { it in allowedSet }) {
+                counter++
+            }
+        }
+
+        return counter
+    }
+}
+
+// Solution 1. (220ms)
 class Solution {
     fun countConsistentStrings(allowed: String, words: Array<String>): Int {
         var counter = 0
