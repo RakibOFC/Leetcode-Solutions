ca2c8e79e58eb606d547cf5fc2149a008eeb215b
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-06-03 15:38:46 +0600

    Add solution for 1512. Number of Good Pairs.kt

---
+++ easy/1512. Number of Good Pairs.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun numIdenticalPairs(nums: IntArray): Int {
+        var count = 0
+
+        for (i in nums.indices) {
+            for (j in nums.indices) {
+                if (nums[i] == nums[j] && i < j) {
+                    count++
+                }
+            }
+        }
+
+        return count
+    }
+}
