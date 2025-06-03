7e5d9a50fe008cb4ec27882ffc9828fa103e5ad3
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-06-03 16:08:32 +0600

    Add solution for 3467. Transform Array by Parity.kt

--- easy/3467. Transform Array by Parity.kt
+++ easy/3467. Transform Array by Parity.kt
@@ -1,3 +1,23 @@
+// Solution 2. (1ms)
+class Solution {
+    fun transformArray(nums: IntArray): IntArray {
+        var evenCount = 0
+        val n = nums.size
+        val arr = IntArray(n)
+
+        for (i in nums.indices) {
+            if (nums[i] % 2 == 0) evenCount++
+        }
+
+        for (i in evenCount..<n) {
+            arr[i] = 1
+        }
+
+        return arr
+    }
+}
+
+// Solution 1. (10ms)
 class Solution {
     fun transformArray(nums: IntArray): IntArray {
 
