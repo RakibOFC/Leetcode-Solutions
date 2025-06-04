e5549b813b7a7d6ac53bc9017e4cb413b0c83a3a
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-06-04 15:21:55 +0600

    Add solution 2 for 1678. Goal Parser Interpretation.kt

--- easy/1678. Goal Parser Interpretation.kt
+++ easy/1678. Goal Parser Interpretation.kt
@@ -1,3 +1,29 @@
+// Solution 2. (96ms)
+class Solution {
+    fun interpret(command: String): String {
+        val result = StringBuilder()
+        var i = 0
+        while (i < command.length) {
+            when {
+                command[i] == 'G' -> {
+                    result.append('G')
+                    i++
+                }
+                command.startsWith("()", i) -> {
+                    result.append('o')
+                    i += 2
+                }
+                command.startsWith("(al)", i) -> {
+                    result.append("al")
+                    i += 4
+                }
+            }
+        }
+        return result.toString()
+    }
+}
+
+// Solution 1. (104ms)
 class Solution {
     fun interpret(command: String): String {
         return command.replace("()", "o").replace("(al)","al")
