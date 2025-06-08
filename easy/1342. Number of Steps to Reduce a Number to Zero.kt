52c961c021fc779d026685c4272e857d2400f8e2
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-06-08 11:34:21 +0600

    Add solution for 1342. Number of Steps to Reduce a Number to Zero.kt

---
+++ easy/1342. Number of Steps to Reduce a Number to Zero.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun numberOfSteps(num: Int): Int {
+        var n = num
+        var count = 0
+
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n /= 2
+            } else {
+                n -= 1
+            }
+            count++
+        }
+
+        return count
+    }
+}
