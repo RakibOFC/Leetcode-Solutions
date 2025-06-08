675c1bce4f964d21f709bfa266b27271f767c62f
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-06-09 05:48:17 +0600

    Add solution 2 for 2843. Count Symmetric Integers.kt

--- easy/2843. Count Symmetric Integers.kt
+++ easy/2843. Count Symmetric Integers.kt
@@ -1,3 +1,39 @@
+// Solution 2. (28ms)
+class Solution {
+    fun countSymmetricIntegers(low: Int, high: Int): Int {
+        var count = 0
+
+        for (n in low..high) {
+            var numLen = 0
+            var num = n
+            val numList = mutableListOf<Int>()
+            
+            while (num > 0) {
+                numList.add(num % 10)
+                num /= 10
+                numLen++
+            }
+
+            if (numLen % 2 == 0) {
+                val lenHalf = numLen / 2
+                var firstHalf = 0
+                var secondHalf = 0
+
+                for (i in 0..<lenHalf) {
+                    firstHalf += numList[i]
+                    secondHalf += numList[i + lenHalf]
+                }
+                if (firstHalf == secondHalf) {
+                    count++
+                }
+            }
+        }
+
+        return count
+    }
+}
+
+// Solution 1. (38ms)
 class Solution {
     fun countSymmetricIntegers(low: Int, high: Int): Int {
         var count = 0
