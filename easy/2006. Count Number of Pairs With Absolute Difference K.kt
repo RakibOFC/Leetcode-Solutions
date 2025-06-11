d081d59e2b09423b70fac46f77ced0f06fd7b2a9
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-06-11 17:53:23 +0600

    Add solution 2 for 2006. Count Number of Pairs With Absolute Difference K.kt

--- easy/2006. Count Number of Pairs With Absolute Difference K.kt
+++ easy/2006. Count Number of Pairs With Absolute Difference K.kt
@@ -1,3 +1,22 @@
+// Solution 2. (7ms)
+class Solution {
+    fun countKDifference(nums: IntArray, k: Int): Int {
+        var count = 0
+
+        for (i in nums.indices) {
+            for (j in i + 1 until nums.size) {
+                val diff = nums[i] - nums[j]
+                if (diff == k || diff == -k) {
+                    count++
+                }
+            }
+        }
+
+        return count
+    }
+}
+
+// Solution 1. (10ms)
 class Solution {
     fun countKDifference(nums: IntArray, k: Int): Int {
         var count = 0
