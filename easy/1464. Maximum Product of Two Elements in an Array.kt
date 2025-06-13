213f97c957f7b7849dbcaed77e0ddd90e577181b
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-06-13 18:42:26 +0600

    Add solution 2 for 1464. Maximum Product of Two Elements in an Array.kt

--- easy/1464. Maximum Product of Two Elements in an Array.kt
+++ easy/1464. Maximum Product of Two Elements in an Array.kt
@@ -1,3 +1,24 @@
+// Solution 2. (0ms)
+class Solution {
+    fun maxProduct(nums: IntArray): Int {
+        var max1 = Int.MIN_VALUE
+        var max2 = Int.MIN_VALUE
+
+        for (num in nums) {
+            if (max1 < num) {
+                max2 = max1
+                max1 = num
+
+            } else if (max2 < num) {
+                max2 = num
+            }
+        }
+
+        return (max1 - 1) * (max2 - 1)
+    }
+}
+
+// Solution 1. (7ms)
 class Solution {
     fun maxProduct(nums: IntArray): Int {
         val n = nums.size
