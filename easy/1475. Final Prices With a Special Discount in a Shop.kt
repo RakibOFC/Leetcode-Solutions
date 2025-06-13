f6b53262a061f53d2e4783c682a018b02e6f85e2
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-06-13 17:02:24 +0600

    Add solution 2 for 1475. Final Prices With a Special Discount in a Shop.kt

--- easy/1475. Final Prices With a Special Discount in a Shop.kt
+++ easy/1475. Final Prices With a Special Discount in a Shop.kt
@@ -1,3 +1,33 @@
+// Solution 2. (2ms)
+class Solution {
+    fun finalPrices(prices: IntArray): IntArray {
+        val n = prices.size
+        val result = IntArray(n)
+        var i = 0
+        var j = 1
+
+        while (i < n) {
+
+            if (j >= n) {
+                result[i] = prices[i]
+                i++
+                j = i + 1
+
+            } else if (i < j && prices[j] <= prices[i]) {
+                result[i] = prices[i] - prices[j]
+                i++
+                j = i + 1
+
+            } else {
+                j++
+            }
+        }
+
+        return result
+    }
+}
+
+// Solution 1. (2ms)
 class Solution {
     fun finalPrices(prices: IntArray): IntArray {
         val result = IntArray(prices.size)
