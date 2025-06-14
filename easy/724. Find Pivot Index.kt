de8ef89bde2123305961cccf19ba60ad78223587
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-06-14 15:30:54 +0600

    Add solution for 724. Find Pivot Index.kt

---
+++ easy/724. Find Pivot Index.kt
@@ -0,0 +1,24 @@
+class Solution {
+    fun pivotIndex(nums: IntArray): Int {
+        val n = nums.size
+        val sumLeftArr = IntArray(n)
+        val sumRightArr = IntArray(n)
+
+        var sumLeft = 0
+        var sumRight = 0
+        for (i in 0..<n) {
+            sumLeft += nums[i]
+            sumRight += nums[n - i - 1]
+
+            sumLeftArr[i] = sumLeft
+            sumRightArr[n - i - 1] = sumRight
+        }
+
+        for (i in 0..<n) {
+            if (sumLeftArr[i] == sumRightArr[i])
+                return i
+        }
+
+        return -1
+    }
+}
