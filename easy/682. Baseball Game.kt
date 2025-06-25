c51b54cf13d538925616f9d3cee56f4bc690042f
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-06-25 11:32:48 +0600

    Add solution 2 for 682. Baseball Game.kt

--- easy/682. Baseball Game.kt
+++ easy/682. Baseball Game.kt
@@ -1,3 +1,37 @@
+// Solution 2.
+class Solution {
+    fun calPoints(operations: Array<String>): Int {
+        val scores = mutableListOf<Int>()
+        var totalPoints = 0
+
+        for (op in operations) {
+            when (op) {
+                "+" -> {
+                    val sum = scores[scores.size - 1] + scores[scores.size - 2]
+                    scores.add(sum)
+                    totalPoints += sum
+                }
+                "D" -> {
+                    val double = scores.last() * 2
+                    scores.add(double)
+                    totalPoints += double
+                }
+                "C" -> {
+                    totalPoints -= scores.removeAt(scores.size - 1)
+                }
+                else -> {
+                    val value = op.toInt()
+                    scores.add(value)
+                    totalPoints += value
+                }
+            }
+        }
+
+        return totalPoints
+    }
+}
+
+// Solution 1.
 class Solution {
     fun calPoints(operations: Array<String>): Int {
         var totalPoint = 0
