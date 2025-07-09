4fa3f99e4e3f7c28da154b3b2c0f9b985b383e57
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-07-09 21:37:29 +0600

    Add solution 219. Contains Duplicate II.kt

---
+++ easy/219. Contains Duplicate II.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun containsNearbyDuplicate(nums: IntArray, k: Int): Boolean {
+        val map = HashMap<Int, Int>()
+
+        for (i in nums.indices) {
+            val num = nums[i]
+            
+            if (map.containsKey(num) && i - map[num]!! <= k) {
+                return true
+            }
+            map[num] = i
+        }
+
+        return false
+    }
+}
